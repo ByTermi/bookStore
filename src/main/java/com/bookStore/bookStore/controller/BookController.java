6dfe7d0c0116616581ae5e3c00fd840cf65b2b89
Jaime <jaimenovillobenito@gmail.com>
2024-07-23 14:42:56 +0200

    home page and database added

---
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookStore.bookStore.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+/**
+ *
+ * @author Vaquin
+ */
+@Controller
+public class BookController {
+    
+    @GetMapping("/")
+    public String home(){
+        return "hola";
+    }
+}
